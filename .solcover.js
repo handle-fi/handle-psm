6cf4f7695b27803657f151347a2d91cee2da47c3
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2022-04-27 10:28:12 +1000

    add coverage ignores

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  skipFiles: [
+    "contracts/interfaces/fxToken.sol",
+    "contracts/interfaces/IFxToken.sol",
+    "contracts/interfaces/IHandle.sol",
+    "contracts/mocks/Handle.sol",
+    "contracts/mocks/MockToken.sol"
+  ]
+};
